508897a304fd153f52a1583a5eb68da22d25e3ac
ThisaraSan <thisarasandaruwan@gmail.com>
2023-12-28 23:08:19 +0530

    create new user UI created

--- src/main/java/lk/ijse/carHire/controller/LoginFormController.java
+++ src/main/java/lk/ijse/carHire/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package lk.ijse.carHire.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public void btnCreateUserAction(ActionEvent actionEvent) throws IOException {
+        //open the Create a New User UI
+        Parent rootCreateNewUser = FXMLLoader.load(this.getClass().getResource("/view/new_user_form.fxml"));
+
+        Scene scene = new Scene(rootCreateNewUser);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/lk/ijse/carHire/controller/NewUserFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.carHire.controller;
+
+public class NewUserFormController {
+}
